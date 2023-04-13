f8c841c2de9969799996731e30077bcabbe118bd
Andriy <andrijcopek696@gmail.com>
2023-04-13 17:18:20 +0300

    UsersCategory functionality

---
+++ TechTreeMVCApplication/Comparers/CompareUsers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using TechTreeMVCApplication.Areas.Admin.Models;
+
+namespace TechTreeMVCApplication.Comparers
+{
+    public class CompareUsers : IEqualityComparer<UserModel>
+    {
+        public bool Equals([AllowNull] UserModel x, [AllowNull] UserModel y)
+        {
+            if (y == null) return false;
+            if (x.Id == y.Id) return true;
+            return false;
+        }
+
+        public int GetHashCode([DisallowNull] UserModel obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
--- TechTreeMVCApplication/Entities/Category.cs
+++ TechTreeMVCApplication/Entities/Category.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using TechTreeMVCApplication.Interfaces;
 
 namespace TechTreeMVCApplication.Entities
 {
-    public class Category
+    public class Category : IPrimaryProperties
     {
         public int Id { get; set; }
         [Required]
