3ebfff25f9376ceddfe424dd8c02dbf0af516278
Andriy <andrijcopek696@gmail.com>
2023-04-15 15:02:38 +0300

    Logged on member

---
+++ TechTreeMVCApplication/Models/CategoryDetailsModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TechTreeMVCApplication.Entities;
+
+namespace TechTreeMVCApplication.Models
+{
+    public class CategoryDetailsModel
+    {
+        public IEnumerable<GroupedCategoryItemsByCategoryModel> GroupedCategoryItemsByCategoryModels { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
